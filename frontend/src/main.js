a138b10c876943f4d6dc7092c06c44d364650813
Pablohenrique2 <pabloenosteteucpx@gmail.com>
2022-07-31 23:17:33 -0300

    feature/route

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
 createApp(App).mount('#app')
+
+Vue.use(VueRouter)
+
+const routes = [
+    { path: '/login', component: login },
+    { path: '/home', component: home },
+  ]
+  const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes, 
+  })
